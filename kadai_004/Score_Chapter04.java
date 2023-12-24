e7ec9c6aaf5fc04721c668c9cb1a0c18d5470529
matsushita16130 <147304798+matsushita16130@users.noreply.github.com>
2023-12-24 15:49:47 +0900

    Add files via upload

---
+++ kadai_004/Score_Chapter04.java
@@ -0,0 +1,31 @@
+package kadai_004;
+
+public class Score_Chapter04 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int score_a = 10;
+		int score_b = 20;
+		int score_c = 30;
+		int score_d = 40;
+		int score_e = 50;
+		int score_f = 60;
+		int score_g = 70;
+		int score_h = 80;
+		int score_i = 90;
+		int score_j = 100;
+		
+		System.out.println("Aさんの点数は" + score_a + "点");
+		System.out.println("Bさんの点数は" + score_b + "点");
+		System.out.println("Cさんの点数は" + score_c + "点");
+		System.out.println("Dさんの点数は" + score_d + "点");
+		System.out.println("Eさんの点数は" + score_e + "点");
+		System.out.println("Fさんの点数は" + score_f + "点");
+		System.out.println("Gさんの点数は" + score_g + "点");
+		System.out.println("Hさんの点数は" + score_h + "点");
+		System.out.println("Iさんの点数は" + score_i + "点");
+		System.out.println("Jさんの点数は" + score_j + "点");
+		System.out.println("テストの平均点は"+((score_a + score_b + score_c + score_d + score_e + score_f + score_g + score_h + score_i + score_j)/10) + "点");
+	}
+
+}
